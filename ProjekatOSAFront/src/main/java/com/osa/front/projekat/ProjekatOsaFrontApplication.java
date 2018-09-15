7315889b02fa00fed8f7b78682b82cea7b921730
freeman <freeman@freeman-ESPRIMO-C700>
2018-09-15 22:18:37 +0200

    inicijalni komit

---
+++ ProjekatOSAFront/src/main/java/com/osa/front/projekat/ProjekatOsaFrontApplication.java
@@ -0,0 +1,12 @@
+package com.osa.front.projekat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjekatOsaFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjekatOsaFrontApplication.class, args);
+	}
+}
